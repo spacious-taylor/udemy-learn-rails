560e900614a6655c300f3e27ab240dad67e440af
taylor-tllaw <tinlok@spacious.hk>
2022-02-15 16:08:26 +0800

    add: Chapter 5 - User Messaging App

---
+++ mailbox/config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  devise_for :users
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+
+  root "conversations#index"
+
+  resources :conversations do
+    resources :messages
+  end
+end
