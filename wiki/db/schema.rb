a3558b4fc95073b70c495317f4e2e95a40b217fa
taylor-tllaw <tinlok@spacious.hk>
2022-02-16 15:12:38 +0800

    7.66: [wiki] Creating Category Migration

--- wiki/db/schema.rb
+++ wiki/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_02_16_035415) do
+ActiveRecord::Schema[7.0].define(version: 2022_02_16_065743) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -18,7 +18,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_16_035415) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id", null: false
+    t.integer "category_id", null: false
+    t.index ["category_id"], name: "index_articles_on_category_id"
     t.index ["user_id"], name: "index_articles_on_user_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -33,5 +41,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_16_035415) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "articles", "categories"
   add_foreign_key "articles", "users"
 end
