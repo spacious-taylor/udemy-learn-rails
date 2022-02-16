470bd38b44f199bd3d8efb2db762699fe1a63824
taylor-tllaw <tinlok@spacious.hk>
2022-02-16 15:25:49 +0800

    7.69: [wiki] Filtering Article View with Category

--- wiki/app/controllers/articles_controller.rb
+++ wiki/app/controllers/articles_controller.rb
@@ -4,7 +4,12 @@ class ArticlesController < ApplicationController
 
   # GET /articles or /articles.json
   def index
-    @articles = Article.all
+    if params[:category].blank?
+      @articles = Article.all
+    else
+      @category_id = Category.find_by(name: params[:category]).id
+      @articles = Article.where(category_id: @category_id)
+    end
   end
 
   # GET /articles/1 or /articles/1.json
@@ -67,6 +72,6 @@ class ArticlesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def article_params
-      params.require(:article).permit(:title, :body, :image_url)
+      params.require(:article).permit(:title, :body, :image_url, :category_id)
     end
 end
